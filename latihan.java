87369e45b95e7760df6c51bf085b8c72af411e33
Aleaatapasya <145308450+Aleaatapasya@users.noreply.github.com>
2023-11-06 08:23:52 +0700

    Update latihan.java

--- latihan.java
+++ latihan.java
@@ -1,24 +1,29 @@
 import java.util.Scanner;
-public class latihan {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        int angka1 = in.nextInt();
-        int jarak = in.nextInt();
-        int ulang = in.nextInt();
 
-        int hasilAritmatika = aritmatika (angka1, jarak, ulang);
-        System.out.println(hasilAritmatika);
-    }
-    private static int hasil =0;
-    private static int aritmatika(int angka1, int jarak, int ulang){
+/**
+ * soal4
+ */
+public class soal4 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Angka Pertama");
+        int angkaAwal = input.nextInt();
+        System.out.println("Masukan Jarak Angka");
+        int jarak = input.nextInt();
+        System.out.println("Masukan Angka Ulang");
+        int ulang = input.nextInt();
+        int hasilarit = aritmatika(angkaAwal, jarak, ulang);
+        System.out.println(hasilarit);
+        input.close();
+    }private static int hasil = 0;
+    private static int aritmatika(int angkaAwal, int jarak , int ulang) {
         if (ulang > 0){
-            ulang = ulang - 1;
-            hasil = hasil + angka1;
-            angka1 = angka1 + jarak;
-            aritmatika(angka1, jarak, ulang);
+            ulang = ulang -1;
+            hasil = hasil + angkaAwal;
+            angkaAwal = angkaAwal + jarak;
+            aritmatika(angkaAwal, jarak, ulang);
         }
         return hasil;
-    }
+    } 
 }
-
